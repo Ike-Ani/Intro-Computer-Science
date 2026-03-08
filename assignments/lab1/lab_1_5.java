4c8641e12b9865e4b72e0876bbf450df269955e2
Ikechukwu Ani <newikejr@gmail.com>
2026-03-08 15:33:30 -0400

    Delete assignments/lab1/lab_1_5.java

--- assignments/lab1/lab_1_5.java
+++
@@ -1,5 +0,0 @@
-public class lab_1_5 {
-    public static void main(String[] args) {
-        System.out.println("John: \"Hey, how's it going?\"\nAlice: \"Pretty good! How about you?\"\"\nJohn: \"I'm great! Let's grab some coffee.\nAlice: \"Sure, let's go!\"");
-    }
-}
